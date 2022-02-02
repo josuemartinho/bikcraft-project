c34a7a8d6ee8d9fbd2e2476d60a3336533b57fb3
Josue Martinho <josuem4rtinho@gmail.com>
2022-02-02 19:23:37 +0000

    projeto final

--- js/script.js
+++ js/script.js
@@ -22,3 +22,44 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicleta
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//animaçao
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
